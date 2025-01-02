a2159fd2ae4ce5ebd3884074d194ba899cfdaf51
imchanyo <chanyoung3320@gmail.com>
2025-01-02 21:04:27 +0900

    배열 회전시키기 / 로직추가 / 기초

--- imchanyo/[week1]Array/Rotate_array.js.js
+++ imchanyo/[week1]Array/Rotate_array.js.js
@@ -11,3 +11,28 @@ function solution(numbers, direction) {
 
   return answer;
 }
+
+/* slice 연습하기 */
+function solution(numbers, direction) {
+  if (direction === "right") {
+    const last = numbers.slice(-1);
+    const rest = numbers.slice(0, -1);
+    return [...last, ...rest];
+  } else if (direction === "left") {
+    const first = numbers.slice(0, 1);
+    const rest = numbers.slice(1);
+    return [...rest, ...first];
+  }
+}
+
+/* splice 연습 */
+function solution(numbers, direction) {
+  if (direction === "right") {
+    const last = numbers.splice(-1, 1);
+    numbers.unshift(...last);
+  } else if (direction === "left") {
+    const first = numbers.splice(0, 1);
+    numbers.push(...first);
+  }
+  return numbers;
+}
