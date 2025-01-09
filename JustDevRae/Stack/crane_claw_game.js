a4143c9bbcdde011ce903f15ec42ad4c373a2e6d
JustDevRae <ksrae165@gmail.com>
2025-01-09 17:45:05 +0900

    crane claw game / 중급

---
+++ JustDevRae/Stack/crane_claw_game.js
@@ -0,0 +1,23 @@
+function solution(board, moves) {
+  let answer = 0;
+  const stack = [];
+
+  for (let move of moves) {
+    for (let i = 0; i < board.length; i++) {
+      if (board[i][move - 1] !== 0) {
+        let doll = board[i][move - 1];
+        board[i][move - 1] = 0;
+
+        if (stack.length > 0 && stack[stack.length - 1] === doll) {
+          stack.pop();
+          answer += 2;
+        } else {
+          stack.push(doll);
+        }
+        break;
+      }
+    }
+  }
+
+  return answer;
+}
