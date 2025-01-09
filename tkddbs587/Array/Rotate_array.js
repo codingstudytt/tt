ac78afe8a29c838c6df7cc2e758dc1dac45c55ae
tkddbs587 <162422277+tkddbs587@users.noreply.github.com>
2025-01-09 14:31:38 +0900

    [tkddbs587] 25.01.02 (#6)
    * Array slicing / 기초
    
    * Length of array elements / 기초
    
    * Rotate array / 기초
    
    * Divisible number array / 중급
    
    * Remove_smallest_number / 중급
    
    * Matrix addition / 중급

---
+++ tkddbs587/Array/Rotate_array.js
@@ -0,0 +1,11 @@
+function solution(numbers, direction) {
+  if (direction === "right") {
+    const lastElement = numbers.pop();
+    numbers.unshift(lastElement);
+  } else if (direction === "left") {
+    const firstElement = numbers.shift();
+    numbers.push(firstElement);
+  }
+
+  return numbers;
+}
