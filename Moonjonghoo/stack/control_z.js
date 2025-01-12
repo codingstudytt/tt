65829ea74fb2d9b11c3ee9c2321d43c86e021959
moonjonghoo <kafmjh12@gmail.com>
2025-01-12 12:54:15 +0900

    control_z / 기초

---
+++ Moonjonghoo/stack/control_z.js
@@ -0,0 +1,8 @@
+function solution(s) {
+  const stack = [];
+  s.split(" ").forEach((target) => {
+    if (target === "Z") stack.pop();
+    else stack.push(+target);
+  });
+  return stack.length ? stack.reduce((pre, cur) => pre + cur) : 0;
+}
